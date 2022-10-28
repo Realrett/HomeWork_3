15fd7c45b5fdeec73c021c73149c5c8dd48f063d
Realrett <gosha-student@mail.ru>
2022-10-28 23:04:32 +0700

    Решил 1 задачу

--- Exersize_1/Program.cs
+++ Exersize_1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+int GetConsole (string message)
+{
+    int result;
+
+    while(true)     
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out int number) && number > 9999 && number < 100000) 
+        {
+            result = number;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число, или не пятизначное число, повторите ввод \n");
+        }
+    }
+
+    return result;
+}
+
+void GetNumber(int number)
+{
+    string Convert1 = Convert.ToString(number);
+    if (Convert1[0] == Convert1[4] && Convert1[1] == Convert1[3])
+    {
+    Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+    Console.WriteLine("Число не является палиндромом");
+    }
+}
+int x = GetConsole("Введите пятизначное число: ");
+GetNumber(x);
